300692917652e5d7f9854c77cd934c5a1e5ac278
korneXD <korcika2005@gmail.com>
2025-01-31 11:10:26 +0100

    Checkout functionality

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -2,21 +2,27 @@ import { createContext } from "react";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import { readCategories, readTransactions } from "../utility/crudUtility";
+import {
+  readCategories,
+  readSettings,
+  readTransactions,
+} from "../utility/crudUtility";
 
 export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
   const [categories, setCategories] = useState(null);
   const [transactions, setTransactions] = useState(null);
+  const [settings, setSettings] = useState(null);
 
   useEffect(() => {
     readCategories(setCategories);
     readTransactions(setTransactions);
+    readSettings(setSettings);
   }, []);
 
   return (
-    <Context.Provider value={{ categories, transactions }}>
+    <Context.Provider value={{ categories, transactions, settings }}>
       {children}
     </Context.Provider>
   );
