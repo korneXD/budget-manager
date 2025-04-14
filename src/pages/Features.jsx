10e472ee42c9a37bb01e7d4a8870f9a317025d10
Patrik930 <focimc02@gmail.com>
2025-04-14 14:39:04 +0200

    Features modified

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { Spotlight } from "../components/Spotlight";
+import Footer from "../components/Footer";
 
 export const Features = () => {
+  useEffect(() => {
+    document.title = "Money Map | Jellemzők";
+  }, []);
+
   const features = [
     {
       name: "Kezelőpanel",
@@ -13,40 +18,49 @@ export const Features = () => {
       desc: "Ezen az oldalon tudsz pénznemet beállítani.",
     },
     {
-      name: "Budget Tips",
+      name: "Tippek",
       desc: "Ezen az oldalon találsz költségvetési tippeket/segédletet.",
     },
     {
-      name: "Faq",
-      desc: "Ezene az oldalon megtalálod a leggyakoribb kérdéseket az oldallal kapcsolatban.",
+      name: "Gyakori kérdések",
+      desc: "Ezen az oldalon megtalálod a leggyakoribb kérdéseket az oldallal kapcsolatban.",
     },
     {
-      name: "Contact",
+      name: "Elérhetőség",
       desc: "Ezen az oldalon megtalálod minden elérhetőségünket.",
     },
     {
-      name: "Why Choose Us",
+      name: "Miért válassz minket?",
       desc: "Ezen az oldalon megtudhatod miért érdemes a mi oldalunkat használnod.",
     },
   ];
 
   return (
     <>
-    <Navbar/>
-        <section id="features" className="min-h-screen w-full flex flex-col items-center justify-center">
-      <h2 className="text-6xl font-nohemi text-sky-200">Our <span className="text-sky-300">Features</span></h2>
-      <span className="text-2xl font-nohemiLight uppercase tracking-wider text-sky-600">Explore our tools</span>
-      <div className="mt-12 grid w-11/12 grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-        {features.map((feature) => (
-          <div key={feature.name} className="flex h-full w-[90%] flex-col items-center justify-center gap-4 rounded-lg border-2 border-sky-950 bg-black/20 p-4 shadow-md backdrop-blur-md">
-            <h4 className="text-2xl font-semibold text-white">{feature.name}</h4>
-            <p className="text-white mt-2">{feature.desc}</p>
-          </div>
-        ))}
-      </div>
-    </section>
-    <Spotlight/>
+      <Navbar />
+      <section
+        id="features"
+        className="mt-32 flex min-h-screen w-full flex-col items-center justify-center md:mt-12"
+      >
+        <h2 className="font-nohemi text-3xl text-white">
+          Az oldal <span className="text-sky-400">Jellemzői</span>
+        </h2>
+        <div className="mt-12 grid w-11/12 grid-cols-1 place-items-center gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          {features.map((feature) => (
+            <div
+              key={feature.name}
+              className="flex h-full w-[90%] flex-col items-center justify-center gap-4 rounded-lg border-2 border-sky-950 bg-black/30 p-4 font-nohemi shadow-md backdrop-blur-sm"
+            >
+              <h4 className="text-2xl font-semibold text-sky-400">
+                {feature.name}
+              </h4>
+              <p className="mt-2 font-nohemiLight text-white">{feature.desc}</p>
+            </div>
+          ))}
+        </div>
+        <Footer />
+      </section>
+      <Spotlight />
     </>
-
   );
 };
